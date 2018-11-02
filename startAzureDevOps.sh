0ff73de8332ccf178bc6745a494ed108633012c1
Fumiya Kume <fumiya.kume@hotmail.com>
2018-11-03 01:19:28 +0900

    Update startAzureDevOps.sh

--- startAzureDevOps.sh
+++ startAzureDevOps.sh
@@ -1,8 +1,14 @@
+echo $1
+echo $2
+echo $3
+echo $4
+ 
 $TOKEN=$1
 $ORGANIZATION=$2
 $POOL_NAME=$3
 $AGENT_NAME=$4
 cd ~
+rm -rf agent
 mkdir agent
 cd agent
 wget https://vstsagentpackage.azureedge.net/agent/2.141.1/vsts-agent-linux-x64-2.141.1.tar.gz
