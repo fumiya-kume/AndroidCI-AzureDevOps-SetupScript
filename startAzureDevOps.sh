054aa2bd24870df31f20fb96a5c0069c8a80eb2f
Fumiya Kume <fumiya.kume@hotmail.com>
2018-11-03 01:39:04 +0900

    Update startAzureDevOps.sh

--- startAzureDevOps.sh
+++ startAzureDevOps.sh
@@ -3,9 +3,6 @@ echo $1
  echo $3
  echo $4
  
- $URL=https://$2.visualstudio.com
- echo $URL
- 
 #$TOKEN=$1
 #$ORGANIZATION=$2
 #$POOL_NAME=$3
@@ -17,5 +14,5 @@ mkdir agent
 cd agent
 wget https://vstsagentpackage.azureedge.net/agent/2.141.1/vsts-agent-linux-x64-2.141.1.tar.gz
 tar zxvf ./vsts-agent-linux-x64-2.141.1.tar.gz
-./config.sh --unattended --url $URL --auth pat --token $1 --pool $3 --agent $4 --acceptTeeEula --runAsService -y
+./config.sh --unattended --url https://$2.visualstudio.com --auth pat --token $1 --pool $3 --agent $4 --acceptTeeEula --runAsService -y
 ./run.sh
