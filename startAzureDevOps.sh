001fb2d4be94f4210536170ffeea04579ac738c7
Fumiya Kume <fumiya.kume@hotmail.com>
2018-11-03 01:50:52 +0900

    Update startAzureDevOps.sh

--- startAzureDevOps.sh
+++ startAzureDevOps.sh
@@ -8,6 +8,13 @@ echo $1
 #$POOL_NAME=$3
 #$AGENT_NAME=$4
 
+sudo ./bin/installdependencies.sh 
+wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install dotnet-sdk-2.1 -y
+
 cd ~
 rm -rf agent
 mkdir agent
