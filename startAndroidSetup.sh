c00a26ea9f930005f73dc157881b63105965b103
Fumiya Kume <fumiya.kume@hotmail.com>
2018-11-03 23:27:00 +0900

    Update startAndroidSetup.sh

--- startAndroidSetup.sh
+++ startAndroidSetup.sh
@@ -5,13 +5,20 @@ SDK_BUILD_TOOLS_VERSION=28.0.2
 sudo apt-get update
 sudo apt-get install default-jre -y
 sudo apt-get install default-jdk -y
-sudo apt-get --quiet update --yes
-sudo apt-get --quiet install --yes wget tar unzip lib32stdc++6 lib32z1
-sudo wget --quiet --output-document=android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-$SDK_VERSION.zip
- unzip -d android-sdk-linux android-sdk.zip
- echo y | android-sdk-linux/tools/bin/sdkmanager "platforms;android-$SDK_BUILD_VERSION" >/dev/null
- echo y | android-sdk-linux/tools/bin/sdkmanager "platform-tools" >/dev/null
- echo y | android-sdk-linux/tools/bin/sdkmanager "build-tools;$SDK_BUILD_TOOLS_VERSION" >/dev/null
- export ANDROID_HOME=$PWD/android-sdk-linux
- export PATH=$PATH:$PWD/android-sdk-linux/platform-tools/
- yes | android-sdk-linux/tools/bin/sdkmanager --licenses
+sudo apt-get --quiet update -y
+sudo apt-get --quiet install -y wget tar unzip lib32stdc++6 lib32z1
+
+ANDROID_ROOT=/usr/local/lib/android
+ANDROID_SDK_ROOT=${ANDROID_ROOT}/sdk
+echo "ANDROID_SDK_ROOT=${ANDROID_SDK_ROOT}" | tee -a /etc/environment
+
+# install Android SDK
+sudo wget  --output-document=android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-$SDK_VERSION.zip
+unzip android-sdk.zip -d ${ANDROID_ROOT}
+rm -f android-sdk.zip
+
+ echo y | ${ANDROID_ROOT}/tools/bin/sdkmanager "platforms;android-$SDK_BUILD_VERSION" >/dev/null
+ echo y | ${ANDROID_ROOT}/tools/bin/sdkmanager "platform-tools" >/dev/null
+ echo y | ${ANDROID_ROOT}/tools/bin/sdkmanager "build-tools;$SDK_BUILD_TOOLS_VERSION" >/dev/null
+# Android SDK のライセンスに同意する
+yes | android-sdk-linux/tools/bin/sdkmanager --licenses
