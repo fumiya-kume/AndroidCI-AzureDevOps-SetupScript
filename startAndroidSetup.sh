662a4fad29ce795d883fe1bd05e86e776804e7c0
Fumiya Kume <fumiya.kume@hotmail.com>
2018-11-04 11:58:55 +0900

    Update startAndroidSetup.sh

--- startAndroidSetup.sh
+++ startAndroidSetup.sh
@@ -3,19 +3,18 @@ SDK_BUILD_VERSION=28
 SDK_BUILD_TOOLS_VERSION=28.0.2
 
 ANDROID_ROOT=/usr/local/lib/android
-ANDROID_HOME=/usr/local/lib/android/tools
 ANDROID_SDK_ROOT=${ANDROID_ROOT}/sdk
 
 echo "ANDROID_SDK_ROOT=${ANDROID_SDK_ROOT}" | sudo tee -a /etc/environment
-echo "ANDROID_HOME=${ANDROID_HOME}" | sudo tee -a /etc/environment
+echo "ANDROID_HOME=${ANDROID_ROOT}" | sudo tee -a /etc/environment
 echo "JAVA_HOME=/usr/bin/java" | sudo tee -a /etc/environment
 
-export ANDROID_HOME=/usr/local/lib/android/tools
-
+export ANDROID_HOME= $ANDROID_ROOT
+export JAVA_HOME= /usr/bin/java
 
 # install Android SDK
-sudo wget  --output-document=android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-${SDK_VERSION}.zip
 sudo apt-get install unzip -y
+sudo wget  --output-document=android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-${SDK_VERSION}.zip
 sudo unzip android-sdk.zip -d ${ANDROID_ROOT}
 rm -f android-sdk.zip
 
