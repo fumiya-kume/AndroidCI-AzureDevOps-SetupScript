32c4bb397b3c0f90f44ad6c0c352fab7f824f0d4
Fumiya Kume <fumiya.kume@hotmail.com>
2018-11-05 14:53:40 +0900

    Update startAndroidSetup.sh

--- startAndroidSetup.sh
+++ startAndroidSetup.sh
@@ -2,19 +2,24 @@ SDK_VERSION=4333796
 SDK_BUILD_VERSION=28
 SDK_BUILD_TOOLS_VERSION=28.0.2
 
-# echo "ANDROID_HOME=${ANDROID_ROOT}" | sudo tee -a /etc/environment
-# echo "JAVA_HOME=/usr/bin/java" | sudo tee -a /etc/environment
+sudo apt install openjdk-8-jdk -y
 
 export ANDROID_HOME=/usr/local/lib/android
-export JAVA_HOME=/usr/lib/jvm/default-java
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/
 
 # install Android SDK
 sudo apt-get install unzip -y
 sudo wget  --output-document=android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-${SDK_VERSION}.zip
-sudo unzip android-sdk.zip -d ${ANDROID_HOME}
+sudo unzip android-sdk.zip -d ${ANDROID_HOME} 
 rm -f android-sdk.zip
+
 echo y | sudo ${ANDROID_HOME}/tools/bin/sdkmanager "platforms;android-$SDK_BUILD_VERSION" >/dev/null
 echo y | sudo ${ANDROID_HOME}/tools/bin/sdkmanager "platform-tools" >/dev/null
 echo y | sudo ${ANDROID_HOME}/tools/bin/sdkmanager "build-tools;$SDK_BUILD_TOOLS_VERSION" >/dev/null
+
+echo y | sudo ${ANDROID_HOME}/tools/bin/sdkmanager "platforms;android-28" >/dev/null
+echo y | sudo ${ANDROID_HOME}/tools/bin/sdkmanager "platform-tools" >/dev/null
+echo y | sudo ${ANDROID_HOME}/tools/bin/sdkmanager "build-tools;28.0.2" >/dev/null
+
 # Android SDK のライセンスに同意する
 yes | sudo ${ANDROID_HOME}/tools/bin/sdkmanager --licenses
