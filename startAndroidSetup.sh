c893409d3b3a5321c828f5dea2ff4b7312744757
Fumiya Kume <fumiya.kume@hotmail.com>
2018-11-06 16:27:48 +0900

    Update startAndroidSetup.sh

--- startAndroidSetup.sh
+++ startAndroidSetup.sh
@@ -1,6 +1,6 @@
 SDK_VERSION=4333796
 SDK_BUILD_VERSION=28
-SDK_BUILD_TOOLS_VERSION=28.0.2
+SDK_BUILD_TOOLS_VERSION=28.0.3
 
 sudo apt install openjdk-8-jdk -y
 
@@ -12,10 +12,11 @@ export PATH=/usr/local/lib/android/tools:/usr/local/lib/android/tools/bin:$PATH
 # install Android SDK
 sudo apt-get install unzip -y
 sudo wget  --output-document=android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-${SDK_VERSION}.zip
-sudo unzip android-sdk.zip -d ${ANDROID_HOME} 
+sudo unzip android-sdk.zip -d ${ANDROID_HOME} -A
 rm -f android-sdk.zip
 
 sudo touch /home/kuxumarin/.android/repositories.cfg
+sudo touch /root/.android/repositories.cfg
 echo y | sudo ${ANDROID_HOME}/tools/bin/sdkmanager "platforms;android-$SDK_BUILD_VERSION" >/dev/null
 echo y | sudo ${ANDROID_HOME}/tools/bin/sdkmanager "platform-tools" >/dev/null
 echo y | sudo ${ANDROID_HOME}/tools/bin/sdkmanager "build-tools;$SDK_BUILD_TOOLS_VERSION" >/dev/null
