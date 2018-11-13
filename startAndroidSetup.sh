1f7281b0d6ccf50acb8b58d3391730429b28cc07
Fumiya Kume <fumiya.kume@hotmail.com>
2018-11-13 19:43:39 +0900

    Update startAndroidSetup.sh

--- startAndroidSetup.sh
+++ startAndroidSetup.sh
@@ -9,6 +9,7 @@ sudo apt-get install unzip -y
 
 # Install Android SDK
 export ANDROID_HOME='/usr/local/lib/android'
+export AndroidSDK='/usr/local/lib/android'
 sudo mkdir /usr/local/lib/android
 cd /usr/local/lib/android
 sudo wget  --output-document=android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
